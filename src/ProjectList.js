60efcd8e092228c346c271c44211226f4731fbf5
DevGibby <71801512+DevGibby@users.noreply.github.com>
2021-10-29 14:22:39 -0400

    Update ProjectList.js

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -78,10 +78,10 @@ export const ProjectList = [
         title:"The Real Deal Fitness",
         linkTitle:"The-Real-Deal-Fitness",
         thumbnail: RDF,
-        paragraph: "The Real Deal Fitness project started out as an example website I was using to learn the Shopify platform. This peaked my interest. I have always been involved in sports and fitness. This is where I began to build the Real Deal Fitness brand. I developed a standalone blog website using React, MongoDb, Express and Node.js. This is a live website to help build my brand using fitness articles, news and more.",
+        paragraph: "The Real Deal Fitness project started out as an example website I was using to learn the Shopify platform and evolved into an entire ecosystem. I have always been involved in sports and fitness. This is where I began to build the Real Deal Fitness brand. I developed a standalone blog website using React, MongoDb, Express and Node.js. This is a live website to help build my brand using fitness articles, news and more.",
         websiteLink: "https://www.trdfit.com/",
         githubLink:"https://github.com/DevGibby/New-Fit-Blog",
-        build:"Built with: JavaScript, React, Redux, MongoDB, Express and Node",
+        build:"Built with: JavaScript, React, Redux, MongoDB, Express, Node and Shopify",
         npm: "Npm Packages: Styled Components and React Router Dom",
     }, 
 ]
